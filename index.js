aef5a8aabb411235e643051e6eed87dd88ba46d4
Ren Blake <rcblake06@gmail.com>
2023-04-10 16:19:07 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks (destinationBlock) {
+    return destinationBlock > 42 ? destinationBlock - 42 : 42 - destinationBlock
+}
+
+function distanceFromHqInFeet (destinationBlock) {
+    return distanceFromHqInBlocks(destinationBlock)*264
+}
+
+function distanceTravelledInFeet (start,destination) {
+    return destination > start ? (destination - start)*264 : (start - destination)*264
+}
+
+function calculatesFarePrice (start,destination) {
+    if (distanceTravelledInFeet (start,destination) < 400 ) {
+        return 0 }
+        else if (distanceTravelledInFeet (start,destination) < 2001) {
+           return ((distanceTravelledInFeet (start,destination)-400)*.02)
+        }
+            else if (distanceTravelledInFeet (start,destination) <2501) {
+              return  25
+            }
+                else { 
+                    return `cannot travel that far`}
+}
